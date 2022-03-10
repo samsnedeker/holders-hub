d1a2f2c648af5ca2d668a4137fbe26daadc00c0c
Sam Snedeker <snedekersa@gmail.com>
2022-03-09 22:53:20 -0700

    header and landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="container">
+        <div className="header-container">
+          <p className="header gradient-text">
+            Welcome to Holder's Hub.
+          </p>
+        </div>
       </header>
     </div>
   );
