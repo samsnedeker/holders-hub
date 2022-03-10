0d6c0c3c7982f1efbe152669db35f9d455d7eecd
Sam Snedeker <snedekersa@gmail.com>
2022-03-10 09:14:25 -0700

    basic frontend + contract validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './styles/App.css';
+import React, { useState } from "react";
+import ContractInputForm from './components/ContractInputForm';
 
 const App = () => {
+
+  const [currContract, setCurrContract] = useState("");
+
+
   return (
     <div className="App">
       <header className="container">
         <div className="header-container">
           <p className="header gradient-text">
-            Welcome to Holder's Hub.
+            Welcome to Holder's Hub
           </p>
+          <p className="sub-text">
+            Import your contract below 
+          </p>
+          <ContractInputForm></ContractInputForm>
         </div>
       </header>
     </div>
---
+++ src/components/ContractInputForm.js
@@ -0,0 +1,64 @@
+import React, { Component, setState } from "react";
+
+class ContractInputForm extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        address: "",
+        error: "",
+        validContract: "",
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleValidation = this.handleValidation.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({ address: event.target.value })
+    }
+  
+    handleSubmit() {
+      if (this.state.validContract === true) {
+        alert("Contract imported successfully!");
+      } else {
+        alert("Contract import failed. Please make sure you input a valid contract address!");
+
+      }
+    }
+
+    handleValidation(event) {
+      event.preventDefault();
+      const address = this.state.address
+      let error = "";
+      let validContract = "";
+
+      if (!(address.length === 42 && address.slice(0, 2) === "0x")) {
+        validContract = false;
+        error = "Invalid contract address!"
+        this.setState( { error: error, validContract: false }, function() {
+          this.handleSubmit();
+        } )
+    } else {
+      this.setState( { validContract: true }, function() {
+        this.handleSubmit();
+      } )
+    }
+  }
+  
+    render() {
+      return (
+        <div>
+          <form onSubmit={this.handleValidation}>
+            <label>
+              Hey
+              <input className="contract-input" placeholder="Contract Address" type="text" address={this.state.address}  onChange={this.handleChange}></input>
+              <input className="cta-button import-contract-button" type="submit" value="Submit" />
+            </label>
+          </form>
+        </div>
+      )
+    }
+  }
+
+export default ContractInputForm
+
